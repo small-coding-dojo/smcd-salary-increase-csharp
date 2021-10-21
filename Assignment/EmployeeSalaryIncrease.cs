4e6c5858d567f02e982e184d052aff0ab910fcb1
Tugbay Atilla <tatilla@igus.net>
2021-10-21 16:49:06 +0200

    mob next [ci-skip] [ci skip] [skip ci]

--- Assignment/EmployeeSalaryIncrease.cs
+++ Assignment/EmployeeSalaryIncrease.cs
@@ -4,36 +4,38 @@ using Moq;
 
 namespace SCD_SalaryIncrease
 {
-    internal class EmployeeSalaryIncrease : IEmployeeSalaryIncrease
-    {
-        private readonly INotify _notify;        
-        private readonly IRepository<Employee> _employeeRepository;
+	internal class EmployeeSalaryIncrease : IEmployeeSalaryIncrease
+	{
+		private readonly INotify _notify;
+		private readonly IRepository<Employee> _employeeRepository;
 
-        public EmployeeSalaryIncrease(INotify notify)
-        {
-            _notify = notify;
-            _employeeRepository = null;
-        }
+		public EmployeeSalaryIncrease(INotify notify)
+		{
+			_notify = notify;
+			_employeeRepository = null;
+		}
 
 
-        public EmployeeSalaryIncrease(INotify notify, IRepository<Employee> employeeRepository)
-        {
-            _notify = notify;
-            _employeeRepository = employeeRepository;
-        }
+		public EmployeeSalaryIncrease(INotify notify, IRepository<Employee> employeeRepository)
+		{
+			_notify = notify;
+			_employeeRepository = employeeRepository;
+		}
 
-        public void IncreaseSalaryByEmail(string email, decimal? percent = null)
-        {
-            if (string.IsNullOrEmpty(email))
-            {
-                throw new ArgumentException(nameof(email));
-            }
+		public void IncreaseSalaryByEmail(string email, decimal? percent = null)
+		{
+			if (string.IsNullOrEmpty(email))
+			{
+				throw new ArgumentException(nameof(email));
+			}
 
-            if ( _employeeRepository != null ) {
-                _employeeRepository.Update(new Employee{CurrentSalary=1450, Email="hugo@example.com"});
-            }
+			var salary = 1450;
+			if (_employeeRepository != null)
+			{
+				_employeeRepository.Update(new Employee { CurrentSalary = salary, Email = email });
+			}
 
-            _notify.NotifySuccess($"{email} salary is manually increased {percent} successfully.");
-        }
-    }
+			_notify.NotifySuccess($"{email} salary is manually increased {percent} successfully.");
+		}
+	}
 }
--- EmployeeSalaryIncreaseTests.cs
+++ EmployeeSalaryIncreaseTests.cs
@@ -59,5 +59,24 @@ namespace SCD_SalaryIncrease
 			employeeRepositoryMock.Verify(m => m.Update(It.IsAny<Employee>()), Times.Once);
 			captured.CurrentSalary.Should().Be(1450);
 		}
+
+		[Test]
+		public void ManualSalaryIncreaseInvokesRepositoryInsert_Duplicated()
+		{
+			Employee captured = null;
+			var employeeRepositoryMock = new Mock<IRepository<Employee>>();
+			employeeRepositoryMock.Setup(m => m.Update(It.IsAny<Employee>())).Callback<Employee>(emp => captured = emp);
+
+			// given an employee with a salary of 1000 and the email address hugo@example.com
+
+			// when calling increaseSalaryByEmail on that employee with an increase of 30
+			var actual = new EmployeeSalaryIncrease(_notifyMock.Object, employeeRepositoryMock.Object);
+			actual.IncreaseSalaryByEmail("hugo@example.com", 30);
+
+			// then the salary of the employee is updated to 1300
+
+			employeeRepositoryMock.Verify(m => m.Update(It.IsAny<Employee>()), Times.Once);
+			captured.CurrentSalary.Should().Be(1300);
+		}
 	}
 }
